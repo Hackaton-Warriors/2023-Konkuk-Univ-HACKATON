4acd349a25c19d5245e28b57ed0203d38e8c6347
ahnjun0 <ahnjun0@pusan.ac.kr>
2023-08-16 17:39:51 +0900

    django API fix

--- Server/newsZip_Django/restAPIlink/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import StringAPI
-
-urlpatterns = [
-    path('api/strings/', StringAPI.as_view(), name='string_api'),
-]
--- Server/newsZip_Django/restAPIlink/views.py
+++ Server/newsZip_Django/restAPIlink/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
-from rest_framework import generics
-from .models import StringModel
-from .serializers import StringSerializer
+# Create your views here
 
-class StringAPI(generics.CreateAPIView):
-    queryset = StringModel.objects.all()
-    serializer_class = StringSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['POST'])
+def check_string(request):
+    data = request.data
+    if data.get('value') == "ABC":
+        return Response("True", status=status.HTTP_200_OK)
+    return Response("False", status=status.HTTP_200_OK)
