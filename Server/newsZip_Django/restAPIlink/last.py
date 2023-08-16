e63f24c7d35f38c606e97a2bd8518ed9b2051903
ahnjun0 <ahnjun0@pusan.ac.kr>
2023-08-16 20:45:03 +0900

    response with JSON (added)

---
+++ Server/newsZip_Django/restAPIlink/last.py
@@ -0,0 +1,48 @@
+from newspaper import Article
+import requests
+from bs4 import BeautifulSoup
+#권한을 얻기위한 헤더
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"}
+
+# url = input()
+url = ""
+def link(url):
+    if "news.naver.com" in url :                    #네이버 뉴스를 크롤링이 안됨으로 별도의 과정이 필요
+        
+        response = requests.get(url, headers = headers)
+
+        html = response.content
+        soup = BeautifulSoup(html, "html.parser")
+        #print(soup)
+
+        print("[제목]")
+        target_element = soup.select_one('h2>span')  # h2 태그 아래의 span 태그 선택, 제목을 추출
+        if target_element:
+            text_content = target_element.get_text().strip()  # 제목을 추출함
+            return text_content
+
+        #articles = soup.select('article')           #기사 본문 내용을 추출
+
+        #print("\n[내용]\n")
+        #for article in articles:
+        #    article_text = article.get_text().strip()  # 요소 내 텍스트 추출
+         #   print(article_text)
+
+    else :
+        html = requests.get(url, headers=headers).text          #naver기사가 아니라면 newspaper3k 패키지를 사용해 제목 기사 추출
+
+        article = Article(url, language='ko')
+        article.download()
+        article.parse()
+
+        # 기사 제목을 출력
+        # print('기사 제목:')
+        #text_content = article.title
+        return article.title
+        
+
+        # 기사 내용을 출력
+        # print('기사 내용:')
+
+        #article_text = article.text
+        #print(article.text)
--- Server/newsZip_Django/restAPIlink/views.py
+++ Server/newsZip_Django/restAPIlink/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
 import sys
-sys.path.append('C:\\Users\\lee\\2023-Konkuk-Univ-HACKATON\\Server\\crawling')
-# from .../crawling/last import link
-from last import link  # 또는 필요한 다른 변수나 함수
+# sys.path.append('C:\\Users\\lee\\2023-Konkuk-Univ-HACKATON\\Server\\crawling')
+from .last import link
+# from last import link  # 또는 필요한 다른 변수나 함수
 
 # 그 외의 views.py 코드...
 
@@ -20,7 +20,12 @@ def check_string(request):
     data = request.data
     url = data.get('value')
     title = link(url)
-    return Response(title, status=status.HTTP_200_OK)
+    abc = "hello"
+    return Response(
+        {
+            "title" : title,
+            "content" : abc
+        }, status=status.HTTP_200_OK)
     
 
 
