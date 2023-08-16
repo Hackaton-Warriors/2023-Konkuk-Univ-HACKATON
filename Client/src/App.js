629eec7a9923c3cfd54f141b6f12853a1cd785b3
dayeon1031 <139859048+dayeon1031@users.noreply.github.com>
2023-08-16 20:50:03 +0900

    QnA update

--- Client/src/App.js
+++ Client/src/App.js
@@ -16,21 +16,48 @@ function HiddenSummary({ title, content, closeSummary }) {
 }
 
 function HiddenQnA({ closeQnA }) {
+  const [question, setQuestion] = useState(''); // 1. 질문 상태 추가
+  const [answer, setAnswer] = useState('Answer will appear here.'); // 응답 상태 추가
+
+  // 2. 입력란에 텍스트를 입력할 때마다 상태를 업데이트
+  const handleInputChange = (e) => {
+    setQuestion(e.target.value);
+  };
+
+  // 3. 백엔드 서버로 질문을 보내고 응답을 받아오는 로직
+  const fetchAnswer = async () => {
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/api/get_answer/', { question });
+      setAnswer(response.data.answer); // 서버에서 받아온 응답을 상태에 저장
+    } catch (error) {
+      console.error("Error fetching answer:", error);
+      setAnswer("Error fetching answer.");
+    }
+  };
+
   return (
     <div className="hidden-QnA">
       <div className="hidden-QnA-content">
         <h2>Q&A with AI</h2>
         <div className="search-container">
-          <input className="search-input" type="text" placeholder="Search in QnA..." />
-          <button className="search-button" type="button">
+          <input 
+            className="search-input" 
+            type="text" 
+            placeholder="Search in QnA..." 
+            value={question} 
+            onChange={handleInputChange}
+          />
+          <button className="search-button" onClick={fetchAnswer} type="button">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
               <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
             </svg>
           </button>
         </div>
         <div className='answer-QnA'>
-          <div style={{ fontWeight: 'bold', fontSize: '25px', padding: "10px 0px" }}>A:</div>Answer is here.</div>
-      </div>
+          <div style={{ fontWeight: 'bold', fontSize: '25px', padding: "10px 0px" }}>A:</div>A:</div>
+          {answer}      
+        </div>
+      
       <button className="close-summary-button" onClick={closeQnA}>Close</button>
     </div>
   );
