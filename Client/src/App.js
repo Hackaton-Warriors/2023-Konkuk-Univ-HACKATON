6bcd3f4377ff246fd1cbe84942a854c77dc62b87
hsung3313 <hsung3313@gmail.com>
2023-08-16 20:06:49 +0900

    hsung(font)

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,13 +1,13 @@
-import React, { useState, useRef, useEffect} from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import axios from 'axios';
 
 function HiddenSummary({ closeSummary }) {
   return (
     <div className="hidden-summary">
       <div className="hidden-summary-content">
-        <h2>Hidden summary Content</h2>
-        <p>This is a hidden summary window that appears when you click on "News Summary".</p>
+        <p style={{fontSize:"30px", fontWeight:"bold"}}>Hidden summary Content</p>
+        <p style={{fontSize:"20px"}}>This is a hidden summary window that appears when you click on "News Summary".</p>
       </div>
       <button className="close-summary-button" onClick={closeSummary}>Close</button>
     </div>
@@ -22,7 +22,7 @@ function HiddenQnA({ closeQnA }) {
         <div className="search-container">
           <input className="search-input" type="text" placeholder="Search in QnA..." />
           <button className="search-button" type="button">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <svg xmlns="http://www.w3.org/2000/svg">
               <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
             </svg>
           </button>
