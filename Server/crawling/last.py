1661fac28dd31cb59297f2e1e696f7260d9b72e3
welsee <leeeunchan28@gmail.com>
2023-08-16 20:25:41 +0900

    thing

--- Server/crawling/last.py
+++ Server/crawling/last.py
@@ -6,39 +6,43 @@ headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36
 
 # url = input()
 url = ""
-if "news.naver.com" in url :                    #네이버 뉴스를 크롤링이 안됨으로 별도의 과정이 필요
-    
-    response = requests.get(url, headers = headers)
+def link(url):
+    if "news.naver.com" in url :                    #네이버 뉴스를 크롤링이 안됨으로 별도의 과정이 필요
+        
+        response = requests.get(url, headers = headers)
 
-    html = response.content
-    soup = BeautifulSoup(html, "html.parser")
-    #print(soup)
+        html = response.content
+        soup = BeautifulSoup(html, "html.parser")
+        #print(soup)
 
-    print("[제목]")
-    target_element = soup.select_one('h2>span')  # h2 태그 아래의 span 태그 선택, 제목을 추출
-    if target_element:
-        text_content = target_element.get_text().strip()  # 제목을 추출함
-        print(text_content)
+        print("[제목]")
+        target_element = soup.select_one('h2>span')  # h2 태그 아래의 span 태그 선택, 제목을 추출
+        if target_element:
+            text_content = target_element.get_text().strip()  # 제목을 추출함
+            return text_content
 
-    articles = soup.select('article')           #기사 본문 내용을 추출
+        #articles = soup.select('article')           #기사 본문 내용을 추출
 
-    print("\n[내용]\n")
-    for article in articles:
-        article_text = article.get_text().strip()  # 요소 내 텍스트 추출
-        print(article_text)
+        #print("\n[내용]\n")
+        #for article in articles:
+        #    article_text = article.get_text().strip()  # 요소 내 텍스트 추출
+         #   print(article_text)
 
-else :
-    html = requests.get(url, headers=headers).text          #naver기사가 아니라면 newspaper3k 패키지를 사용해 제모그 기사 추출
+    else :
+        html = requests.get(url, headers=headers).text          #naver기사가 아니라면 newspaper3k 패키지를 사용해 제목 기사 추출
 
-    article = Article(url, language='ko')
-    article.download()
-    article.parse()
+        article = Article(url, language='ko')
+        article.download()
+        article.parse()
 
-    # 기사 제목을 출력
-    # print('기사 제목:')
-    print(article.title)
-    print('')
+        # 기사 제목을 출력
+        # print('기사 제목:')
+        #text_content = article.title
+        return article.title
+        
 
-    # 기사 내용을 출력
-    # print('기사 내용:')
-    print(article.text)
+        # 기사 내용을 출력
+        # print('기사 내용:')
+
+        #article_text = article.text
+        #print(article.text)
--- Server/newsZip_Django/restAPIlink/views.py
+++ Server/newsZip_Django/restAPIlink/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-from crawling import last
+
+import sys
+sys.path.append('C:\\Users\\lee\\2023-Konkuk-Univ-HACKATON\\Server\\crawling')
+# from .../crawling/last import link
+from last import link  # 또는 필요한 다른 변수나 함수
+
+# 그 외의 views.py 코드...
+
+
 
 # Create your views here
 
@@ -11,6 +19,10 @@ from rest_framework.response import Response
 def check_string(request):
     data = request.data
     url = data.get('value')
+    title = link(url)
+    return Response(title, status=status.HTTP_200_OK)
+    
+
 
     #last.py
 
